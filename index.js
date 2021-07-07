c9fcf635bb4ebff2084200d72fb370884f625a54
Nguyen Van Minh Toi <toi.nguyen.vtp@enjoyworks.co.kr>
2021-07-07 16:32:34 +0700

    init package

---
+++ index.js
@@ -0,0 +1,8 @@
+function awesomeEmojiLog(message) {
+  if (message === undefined) throw new Error('No Message Found');
+  console.log('😎', message);
+}
+
+module.exports = awesomeEmojiLog;
+console.log('Hello world');
+awesomeEmojiLog('This is awesome emoji');
